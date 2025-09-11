29da9e2fb306db03448fa30a990962728342e4b4
Guilherme Domingues <guilherme.ldomingues@gmail.com>
2025-09-11 11:04:35 -0300

    Add files via upload

---
+++ projetoMetodo2/projetoMetodo2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace projetoMetodo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSaida = new System.Windows.Forms.Label();
+            this.lblValidarData = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUESTÃO 5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LOGIN";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(40, 73);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SENHA";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(40, 112);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ENTRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSaida
+            // 
+            this.lblSaida.AutoSize = true;
+            this.lblSaida.Location = new System.Drawing.Point(70, 178);
+            this.lblSaida.Name = "lblSaida";
+            this.lblSaida.Size = new System.Drawing.Size(0, 13);
+            this.lblSaida.TabIndex = 6;
+            // 
+            // lblValidarData
+            // 
+            this.lblValidarData.AutoSize = true;
+            this.lblValidarData.Location = new System.Drawing.Point(269, 220);
+            this.lblValidarData.Name = "lblValidarData";
+            this.lblValidarData.Size = new System.Drawing.Size(0, 13);
+            this.lblValidarData.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "VALIDAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(253, 112);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(100, 20);
+            this.txtMes.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "MÊS";
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(253, 73);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(100, 20);
+            this.txtDia.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(283, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "DIA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(256, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "QUESTÃO 5";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(253, 154);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(100, 20);
+            this.txtAno.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(283, 138);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "ANO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblValidarData);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblSaida);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSaida;
+        private System.Windows.Forms.Label lblValidarData;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ projetoMetodo2/projetoMetodo2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetoMetodo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = (txtLogin.Text);
+            string senha = (txtSenha.Text);
+            lblSaida.Text = confirma(login, senha);
+        }
+        private string verificacao(string l, string s)
+        {
+            char[] checkArray = l.ToCharArray();
+            string invertido = String.Empty;
+            for (int i = checkArray.Length - 1; i >= 0; i--)
+            {
+                invertido += checkArray[i];
+            }
+            return invertido;
+        }
+        private string confirma(string l, string s)
+        {
+            char[] checkArray = l.ToCharArray();
+            string invertido = String.Empty;
+            for (int i = checkArray.Length - 1; i >= 0; i--)
+            {
+                invertido += checkArray[i];
+            }
+            if (invertido != s)
+            {
+                return "Perfil Inexistente";
+            }
+            else
+            {
+                return "Bem vindo!";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int dia = int.Parse(txtDia.Text);
+            int mes = int.Parse(txtMes.Text);
+            int ano = int.Parse(txtAno.Text);
+            lblValidarData.Text = validacao(dia, mes, ano);
+        }
+        private string validacao(int d, int m, int a)
+        {
+            string verificacao;
+            if (d > 0 && d <= 30 && m > 0 && m <= 12 && a >= 0)
+            {
+                verificacao = "É uma data válida";
+            }
+            else
+            {
+                verificacao = "Essa data não existe";
+            }
+
+            return verificacao;
+        }
+    }
+}
